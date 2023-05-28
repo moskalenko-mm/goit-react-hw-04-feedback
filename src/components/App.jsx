620fd98b86a96dde2696131056d9ff7a7413a9cd
Nicholas <nicholasmoskalenko@gmail.com>
2023-05-28 15:16:18 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,13 +15,13 @@ const App = () => {
   const handleButtonClick = item => {
     switch (item) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevGood => prevGood + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevNeutral => prevNeutral + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevBad => prevBad + 1);
         break;
 
       default:
